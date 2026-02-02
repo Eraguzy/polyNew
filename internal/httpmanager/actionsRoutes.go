d0517ee00ee43db1cdd844fe568f7374c1e61d0a
Eraguzy <122539649+Eraguzy@users.noreply.github.com>
2026-02-02 23:48:12 +0100

    add : basic auth for db routes

--- internal/httpmanager/actionsRoutes.go
+++ internal/httpmanager/actionsRoutes.go
@@ -7,11 +7,11 @@ import (
 func UpdaterRoutes(mux *http.ServeMux, db DBHandler) *http.ServeMux {
 	basePath := "/actions"
 	//tags
-	mux.HandleFunc(basePath+"/fetch-store-tag", db.HandlerFetchStoreTag)
-	mux.HandleFunc(basePath+"/set-tracked-tag", db.HandlerSetTrackedTag)
-	mux.HandleFunc(basePath+"/notify-tracked-tags", db.HandlerNotifyTrackedTags)
+	mux.HandleFunc(basePath+"/fetch-store-tag", BasicAuth(db.HandlerFetchStoreTag))
+	mux.HandleFunc(basePath+"/set-tracked-tag", BasicAuth(db.HandlerSetTrackedTag))
+	mux.HandleFunc(basePath+"/notify-tracked-tags", BasicAuth(db.HandlerNotifyTrackedTags))
 
-	mux.HandleFunc(basePath+"/compare-events", db.HandlerCompareEvents)
+	mux.HandleFunc(basePath+"/compare-events", BasicAuth(db.HandlerCompareEvents))
 
 	return mux
 }
--- internal/httpmanager/httpManager.go
+++ internal/httpmanager/httpManager.go
@@ -1,7 +1,30 @@
 package httpmanager
 
-import "github.com/Eraguzy/PolyNew/internal/storage"
+import (
+	"net/http"
+	"os"
+
+	"github.com/Eraguzy/PolyNew/internal/storage"
+)
 
 type DBHandler struct {
 	Db storage.DBManager
+}
+
+func BasicAuth(
+	handler func(w http.ResponseWriter, r *http.Request),
+) func(w http.ResponseWriter, r *http.Request) {
+
+	var user = os.Getenv("BASICAUTH_USER")
+	var pass = os.Getenv("BASICAUTH_PASS")
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, p, ok := r.BasicAuth()
+		if !ok || u != user || p != pass {
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted"`)
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		handler(w, r)
+	}
 }
